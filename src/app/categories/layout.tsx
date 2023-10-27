c3b062195806a156606e5aa553f0f12b513bd2bb
Konrad <115343176+blackweeed@users.noreply.github.com>
2023-10-27 06:54:08 +0000

    delated category page

--- src/app/categories/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { type ReactNode } from "react";
-
-export const generateStaticParams = async () => {
-	return [{ category: "shoes" }, { category: "skateboards" }];
-};
-
-export default function CategoryProductLayout({ children }: { children: ReactNode }) {
-	return children;
-}
